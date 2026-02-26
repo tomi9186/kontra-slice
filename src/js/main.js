95d8e6eeeb71c2a36bf4b3c377ef90655aaf5add
Tomislav Suk <suk.tomislav@gmail.com>
2026-02-26 13:21:35 +0100

    Kontra Slice v2

--- src/js/main.js
+++ src/js/main.js
@@ -3,35 +3,3 @@ import '../styles/main.scss';
 
 // Bootstrap JS (bundle uključuje Popper)
 import 'bootstrap/dist/js/bootstrap.bundle';
-
-// Custom JS
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('WP Slice Starter loaded');
-
-  // Navbar hamburger menu custom tekst
-  const navbarToggler = document.querySelector('.navbar-toggler');
-  const navbarCollapse = document.querySelector('.navbar-collapse');
-  const body = document.body;
-
-  if (navbarToggler && navbarCollapse) {
-    // Dodaj klasu kada je navigacija otvorena (sprječava scrollanje)
-    navbarCollapse.addEventListener('show.bs.collapse', () => {
-      body.classList.add('navbar-open');
-    });
-
-    navbarCollapse.addEventListener('hide.bs.collapse', () => {
-      body.classList.remove('navbar-open');
-    });
-
-    // Zatvori navigaciju nakon klika na nav link
-    const navLinks = navbarCollapse.querySelectorAll('.nav-link');
-    navLinks.forEach(link => {
-      link.addEventListener('click', () => {
-        const bsCollapse = new (window.bootstrap.Collapse)(navbarCollapse, {
-          toggle: false
-        });
-        bsCollapse.hide();
-      });
-    });
-  }
-});
